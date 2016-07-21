4b144f4189e15ed95c4918e4beaf7dc07279f278
Alejandro Nanez <alejonanez@gmail.com>
2016-07-21 18:18:03 -0500

    Classes

--- OOP_structs.playground/Contents.swift
+++ OOP_structs.playground/Contents.swift
@@ -32,3 +32,63 @@ struct Point {
 let coordinatePoint = Point(x: 0, y: 0)
 
 let a = coordinatePoint.surroundingPoints(withRange: 1)
+
+class Enemy {
+    var life: Int = 2
+    let position: Point
+    
+    init(x: Int, y: Int) {
+        self.position = Point(x: x, y: y)
+    }
+    
+    func decreaseHealth(factor: Int) {
+        life -= factor
+    }
+}
+
+class Tower {
+    let position: Point
+    var range: Int = 1
+    var strength: Int = 1
+    
+    init(x: Int, y: Int) {
+        self.position = Point(x: x, y: y)
+    }
+    
+    func fireAtEnemy(enemy: Enemy) {
+        if inRange(self.position, range: self.range, target: enemy.position) {
+            while enemy.life > 0 {
+                enemy.decreaseHealth(self.strength)
+                print("Enemy vanquished")
+            }
+        } else {
+            print("The enemy is out of range")
+        }
+    }
+    
+    func inRange(position: Point, range: Int, target: Point) -> Bool {
+        let availablePositions = position.surroundingPoints(withRange: range)
+        
+        for point in availablePositions {
+            if (point.x == target.x) && (point.y == target.y) {
+                return true
+            }
+        }
+        
+        return false
+    }
+}
+
+let tower = Tower(x: 0, y: 0)
+let enemy = Enemy(x: 1, y: 1)
+
+tower.fireAtEnemy(enemy)
+
+
+
+
+
+
+
+
+
